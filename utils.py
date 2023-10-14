658a5180cd2b5988da88c7884822a7df0c281ae3
dmitriyalushkin <pantific37@mail.ru>
2023-10-14 21:18:29 +0300

    Add class HeadHunterAPI, SuperJobAPI

--- utils.py
+++ utils.py
@@ -25,6 +25,61 @@ class Vacancy:
         return f'{self.name}'
 
 
+class HeadHunterAPI(Vacancy, ApiClient):
+    """Получает вакансию от API HeadHunter"""
+
+    def __init__(self, name, page, top_n):
+            super().__init__(name, page, top_n)
+            self.url = 'https://api.hh.ru'
+
+    def get_vacancies(self):
+            data = requests.get(f'{self.url}/vacancies', params={'text': 'python', 'page': 1, 'per_page': 10}).json()
+            return data
+
+    def add_vacancy_hh(self):
+        data = self.get_vacancies()
+        vacancies = []
+        for vacancy in data['items']:
+            head_hunter = {
+                'id': vacancy['id'],
+                'name': vacancy['name'],
+                'salary_ot': vacancy['salary']['from'] if vacancy.get('salary') else None,
+                'salary_do': vacancy['salary']['to'] if vacancy.get('salary') else None,
+                'responsibility': vacancy['snippet']['responsibility']
+            }
+            vacancies.append(head_hunter)
+
+        return vacancies
+
+
+class SuperJobAPI(Vacancy, ApiClient):
+    """Получает вакансию от API SuperJob"""
+    def __init__(self, name, page, top_n):
+        super().__init__(name, page, top_n)
+        self.url = 'https://api.superjob.ru/2.0/vacancies/'
+
+    def get_vacancies(self):
+        headers = {
+                        'X-Api-App-Id': os.getenv('SUPERJOB_API_KEY'),
+                    }
+        data = requests.get(self.url, headers=headers, params={'keywords': 'python', 'page': 1, 'count': 10}).json()
+        return data
+
+    def add_vacancy_sj(self):
+        data = self.get_vacancies()
+        vacancy_list_SJ = []
+        for i in data['objects']:
+            super_job = {
+                        'id': i['id'],
+                        'name': i.get('profession', ''),
+                        'salary_ot': i.get('payment_from', '') if i.get('payment_from') else None,
+                        'salary_do': i.get('payment_to') if i.get('payment_to') else None,
+                        'responsibility': i.get('candidat').replace('\n', '').replace('•', '') if i.get('candidat') else None
+                    }
+            vacancy_list_SJ.append(super_job)
+            return vacancy_list_SJ
+
+
 
 
 
