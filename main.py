708f591028f1dcad151b60ad353df39df1cc04c4
dmitriyalushkin <pantific37@mail.ru>
2023-10-23 19:49:30 +0300

    Add changes class Vacancy

--- main.py
+++ main.py
@@ -1,59 +1,41 @@
-from utils import HeadHunterAPI, SuperJobAPI
+from utils import HeadHunterAPI, SuperJobAPI, JSONSaver
 import json
 
 def user_interaction():
     '''Функция для взаимодействия с пользователем'''
 
     name = input('Введите вакансию: ')
-    top_n = input('Введите кол-во вакансий: ')
-    page = int(input('Введите страницу: '))
+    page = input('Введите кол-во вакансий: ')
+    per_page = int(input('Введите страницу: '))
 
     platforma = input('введите платформу для поиска: (1 - HH, 2 - SJ, 3 - обе платформы)  ')
 
     if platforma == '3':
         while True:
-            hh_instance = HeadHunterAPI(name, page, top_n)
-            sj_instance = SuperJobAPI(name, page, top_n)
-            combined_dict = {'HH': hh_instance.add_vacancy_hh(), 'SJ': sj_instance.add_vacancy_sj()}
-            hh_instance.page = page
-            sj_instance.page = page
-            hh_data = hh_instance.add_vacancy_hh()
-            sj_data = sj_instance.add_vacancy_sj()
+            hh_instance = HeadHunterAPI(name, page, per_page)
+            sj_instance = SuperJobAPI(name, page, per_page)
+            vacancies = {'HH': hh_instance.add_vacancy_hh(), 'SJ': sj_instance.add_vacancy_sj()}
 
-            combined_dict['HH'] = hh_data
-            combined_dict['SJ'] = sj_data
-
-            with open('vacancies.json', 'w', encoding='utf-8') as file:
-                json.dump(combined_dict, file, ensure_ascii=False, indent=2)
-
-            for platform, data in combined_dict.items():
-                print(f"\n \033Платформа: {platform}")
+            for platform, data in vacancies.items():
+                print(f"Платформа: {platform}")
                 for item in data:
-                    print(
-                        f"id - {item['id']}\nДолжность - {item['name']}\nЗ.п от - {item['salary_ot']}\nЗ.п до - {item['salary_do']}\nОписание - {item['responsibility']}\n")
+                    print(item)
 
             a = input('перейти на следующую страницу? y/n ')
             if a == 'y':
                 page += 1
             else:
                 break
+
     elif platforma == '1':
         while True:
-            hh_instance = HeadHunterAPI(name, page, top_n)
-            sj_instance = SuperJobAPI(name, page, top_n)
-            combined_dict = {'HH': hh_instance.add_vacancy_hh(), 'SJ': sj_instance.add_vacancy_sj()}
-            hh_instance.page = page
-            sj_instance.page = page
-            hh_data = hh_instance.add_vacancy_hh()
+            hh_instance = HeadHunterAPI(name, page, per_page)
+            vacancies = hh_instance.add_vacancy_hh()
+            json_saver = JSONSaver()
+            json_saver.add_vacancies(vacancies, 'hh.json')
 
-            combined_dict['HH'] = hh_data
-
-            with open('vacancies.json', 'w', encoding='utf-8') as file:
-                json.dump(combined_dict, file, ensure_ascii=False, indent=2)
-
-            for platform in combined_dict['HH']:
-                print(
-                    f"\nid - {platform['id']}\nДолжность - {platform['name']}\nЗ.п от - {platform['salary_ot']}\nЗ.п до - {platform['salary_do']}\nОписание - {platform['responsibility']}\n")
+            for vacancy in vacancies:
+                print(vacancy)
             a = input('перейти на следующую страницу? y/n ')
             if a == 'y':
                 page += 1
@@ -62,23 +44,13 @@ def user_interaction():
 
     elif platforma == '2':
         while True:
-            hh_instance = HeadHunterAPI(name, page, top_n)
-            sj_instance = SuperJobAPI(name, page, top_n)
-            combined_dict = {'HH': hh_instance.add_vacancy_hh(), 'SJ': sj_instance.add_vacancy_sj()}
-            hh_instance.page = page
-            sj_instance.page = page
-            hh_data = hh_instance.add_vacancy_hh()
-            sj_data = sj_instance.add_vacancy_sj()
+            sj_instance = SuperJobAPI(name, page, per_page)
+            vacancies = sj_instance.add_vacancy_sj()
+            json_saver = JSONSaver()
+            json_saver.add_vacancies(vacancies, 'sj.json')
 
-            combined_dict['HH'] = hh_data
-            combined_dict['SJ'] = sj_data
-
-            with open('vacancies.json', 'w', encoding='utf-8') as file:
-                json.dump(combined_dict, file, ensure_ascii=False, indent=2)
-
-            for platform in combined_dict['SJ']:
-                print(
-                    f"\nid - {platform['id']}\nДолжность - {platform['name']}\nЗ.п от - {platform['salary_ot']}\nЗ.п до - {platform['salary_do']}\nОписание - {platform['responsibility']}\n")
+            for vacancy in vacancies:
+                print(vacancy)
 
             a = input('перейти на следующую страницу? y/n ')
             if a == 'y':
@@ -87,6 +59,6 @@ def user_interaction():
                 break
 
 user_interaction()
-hh_api = HeadHunterAPI('python', 1, 1)
-superjob_api = SuperJobAPI('python', 1, 100000)
-print(hh_api >= superjob_api)
+# hh_api = HeadHunterAPI('python', 1, 1)
+# superjob_api = SuperJobAPI('python', 1, 100000)
+# print(hh_api >= superjob_api)
--- utils.py
+++ utils.py
@@ -16,109 +16,133 @@ class ApiClient(ABC):
 
 
 class Vacancy:
-    def __init__(self, name, page, top_n):
+    '''Класс для работы с вакансиями'''
+
+    def __init__(self, id, name, salary_ot, salary_do, responsibility):
+        self.id = id
         self.name = name
-        self.page = page
-        self.top_n = top_n
+        self.salary_ot = salary_ot
+        self.salary_do = salary_do
+        self.responsibility = responsibility
 
-
-    def __repr__(self):
-        return f'{self.name}'
+    def __str__(self):
+        return f"\nid - {self.id}\nДолжность - {self.name}\n" \
+               f"З.п от - {self.salary_ot}\nЗ.п до - {self.salary_do}\n" \
+               f"Описание - {self.responsibility}\n"
 
     def __ge__(self, other):
-        return self.top_n >= other.top_n
+        return self.salary_ot >= other.salary_ot
+
+    def to_json(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'salary_ot': self.salary_ot,
+            'salary_do': self.salary_do,
+            'responsibility': self.responsibility
+        }
 
 
-
-class HeadHunterAPI(Vacancy, ApiClient):
+class HeadHunterAPI(ApiClient):
     """Получает вакансию от API HeadHunter"""
 
-    def __init__(self, name, page, top_n):
-            super().__init__(name, page, top_n)
+    def __init__(self, name, page, per_page):
+            self.name = name
+            self.page = page
+            self.per_page = per_page
             self.url = 'https://api.hh.ru'
 
     def get_vacancies(self):
-            data = requests.get(f'{self.url}/vacancies', params={'text': self.name, 'page': self.page, 'per_page': self.top_n}).json()
+            data = requests.get(f'{self.url}/vacancies', params={'text': self.name,
+                                                                 'page': self.page,
+                                                                 'per_page': self.per_page}).json()
             return data
 
     def add_vacancy_hh(self):
         data = self.get_vacancies()
         vacancies = []
-        for vacancy in data['items']:
-            head_hunter = {
-                'id': vacancy['id'],
-                'name': vacancy['name'],
-                'salary_ot': vacancy['salary']['from'] if vacancy.get('salary') else None,
-                'salary_do': vacancy['salary']['to'] if vacancy.get('salary') else None,
-                'responsibility': vacancy['snippet']['responsibility']
-            }
-            vacancies.append(head_hunter)
+        for hh_vacancy in data['items']:
+            vacancy = Vacancy(id=hh_vacancy['id'],
+                              name=hh_vacancy['name'],
+                              salary_ot=hh_vacancy['salary']['from'] if hh_vacancy['salary'] else None,
+                              salary_do=hh_vacancy['salary']['to'] if hh_vacancy['salary'] else None,
+                              responsibility=hh_vacancy['snippet']['requirement'])
+            vacancies.append(vacancy)
 
         return vacancies
 
 
-class SuperJobAPI(Vacancy, ApiClient):
+class SuperJobAPI(ApiClient):
     """Получает вакансию от API SuperJob"""
-    def __init__(self, name, page, top_n):
-        super().__init__(name, page, top_n)
+    def __init__(self, name, page, per_page):
+        self.name = name
+        self.page = page
+        self.per_page = per_page
         self.url = 'https://api.superjob.ru/2.0/vacancies/'
 
     def get_vacancies(self):
         headers = {
                     'X-Api-App-Id': os.getenv('X-Api-App-Id'),
                     }
-        data = requests.get(self.url, headers=headers, params={'keywords': self.name, 'page': self.page, 'count': self.top_n}).json()
+        data = requests.get(self.url, headers=headers, params={'keywords': self.name,
+                                                               'page': self.page,
+                                                               'count': self.per_page}).json()
         return data
 
     def add_vacancy_sj(self):
         data = self.get_vacancies()
         vacancy_list_SJ = []
-        for i in data['objects']:
-            super_job = {
-                        'id': i['id'],
-                        'name': i.get('profession', ''),
-                        'salary_ot': i.get('payment_from', '') if i.get('payment_from') else None,
-                        'salary_do': i.get('payment_to') if i.get('payment_to') else None,
-                        'responsibility': i.get('candidat').replace('\n', '').replace('•', '') if i.get('candidat') else None
-                    }
-            vacancy_list_SJ.append(super_job)
+        for sj_vacancy in data['objects']:
+            vacancy = Vacancy(
+                id=sj_vacancy['id'],
+                name=sj_vacancy['profession'],
+                salary_ot=sj_vacancy['payment_from'] if sj_vacancy['payment_from'] else None,
+                salary_do=sj_vacancy['payment_to'] if sj_vacancy['payment_to'] else None,
+                responsibility=sj_vacancy['candidat']
+            )
+            vacancy_list_SJ.append(vacancy)
             return vacancy_list_SJ
 
 
 
 class VacanciesJSON(ABC):
     @abstractmethod
-    def add_vacancy_headhunter(self):
+    def add_vacancies(self, data, filename):
         pass
+
 
     @abstractmethod
-    def add_vacancy_superjob(self):
+    def add_vacancy(self, vacancy, filename):
         pass
 
+
 class JSONSaver(VacanciesJSON):
-    def add_vacancy_headhunter(self, data):
-        with open('hh.json', 'a', encoding='utf-8') as file:
-            data_hh = json.dump(data, file, indent=2, ensure_ascii=False)
-        return data_hh
+    '''Класс для сохранения информации о вакансиях в JSON-файл'''
+    def add_vacancies(self, vacancies, filename):
 
-    def add_vacancy_superjob(self, data):
-        with open('sj.json', 'a', encoding='utf-8') as file:
-            data_sj = json.dump(data, file, indent=2, ensure_ascii=False)
-        return data_sj
+        for vacancy in vacancies:
+            self.add_vacancy(vacancy, filename)
 
-
-
+    def add_vacancy(self, vacancy, filename):
+        with open(filename, 'w', encoding='utf-8') as file:
+            data = json.dump(vacancy.to_json(), file, indent=2, ensure_ascii=False)
+        return data
 
 
 
 
 
 
-hh_api = HeadHunterAPI('python', 1, 1)
+
+
+
+
+
+# hh_api = HeadHunterAPI('python', 1, 1)
 # hh_vacancies = hh_api.get_vacancies()
 # print(hh_vacancies)
-superjob_api = SuperJobAPI('python', 1, 100000)
-# superjob_vacancies = superjob_api.add_vacancy_sj()
+# superjob_api = SuperJobAPI('python', 1, 100000)
+# superjob_vacancies = superjob_api.get_vacancies()
 # print(superjob_vacancies)
 # jsonsaver = JSONSaver()
 # print(jsonsaver.add_vacancy_headhunter(hh_vacancies))
